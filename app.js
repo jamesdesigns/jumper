f8f2f5397dd1a8edb5ec9eb947f19c72509149a2
James Hooper <jameshooperdesigns@gmail.com>
2020-12-05 15:00:38 -0700

    Add left, right, and straight functions to the app

--- app.js
+++ app.js
@@ -101,21 +101,46 @@ document.addEventListener('DOMContentLoaded', () => {
     function control(e) {
         if (e.key === "ArrowLeft") {
             moveLeft()
-
         } else if (e.key === "ArrowRight") {
-            // Move right
-
+            moveRight()
         } else if (e.key === "ArrowUp") {
             // moveStraight
         }
     }
 
     function moveLeft() {
+        if (isGoingRight) {
+            clearInterval(rightTimerId)
+            isGoingRight = false
+        }
         isGoingLeft = true
         leftTimerId - setInterval(function() {
-            doodlerLeftSpace -=5
-            doodler.style.left = doodlerLeftSpace + 'px'
+            if (doodlerLeftSpace >= 0) {
+                doodlerLeftSpace -=5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveRight()
         }, 30)
+    }
+
+    function moveRight() {
+        if (isGoingLeft) {
+            clearInterval(leftTimerId)
+            isGoingLeft = false
+        }
+        isGoingRight = true
+        rightTimerId = setInterval(function() {
+            if (doodlerLeftSpace <= 340) {
+                doodlerLeftSpace += 5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveLeft()
+        }, 30)
+    }
+
+    function moveStraight() {
+        isGoingRight = false
+        isGoingLeft = false
+        clearInterval(rightTimerId)
+        clearInterval(leftTimerId)
     }
 
     function start() {
@@ -124,6 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
             createDoodler()
             setInterval(movePlatforms, 30)
             jump()
+            document.addEventListener('keyup', control)
         }
     }
     // Attach to button
